3c9449a3839b90f8973b010e3af3e9feb6e286a1
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2023-08-12 20:52:19 -0700

    Create aslfr.py

---
+++ alz/aslfr.py
@@ -0,0 +1,214 @@
+import os
+import gc
+import json
+import math
+import pickle
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow_addons as tfa
+IS_INTERACTIVE = os.environ['KAGGLE_KERNEL_RUN_TYPE'] == 'Interactive'
+
+
+class ECA(tf.keras.layers.Layer):
+    def __init__(self, kernel_size=5, **kwargs):
+        super().__init__(**kwargs)
+        self.supports_masking = True
+        self.kernel_size = kernel_size
+        self.conv = tf.keras.layers.Conv1D(1, kernel_size=kernel_size, strides=1, padding="same", use_bias=False)
+
+    def call(self, inputs, mask=None):
+        nn = tf.keras.layers.GlobalAveragePooling1D()(inputs, mask=mask)
+        nn = tf.expand_dims(nn, -1)
+        nn = self.conv(nn)
+        nn = tf.squeeze(nn, -1)
+        nn = tf.nn.sigmoid(nn)
+        nn = nn[:,None,:]
+        return inputs * nn
+
+class CausalDWConv1D(tf.keras.layers.Layer):
+    def __init__(self, 
+        kernel_size=17,
+        dilation_rate=1,
+        use_bias=False,
+        depthwise_initializer='glorot_uniform',
+        name='', **kwargs):
+        super().__init__(name=name,**kwargs)
+        self.causal_pad = tf.keras.layers.ZeroPadding1D((dilation_rate*(kernel_size-1),0),name=name + '_pad')
+        self.dw_conv = tf.keras.layers.DepthwiseConv1D(
+                            kernel_size,
+                            strides=1,
+                            dilation_rate=dilation_rate,
+                            padding='valid',
+                            use_bias=use_bias,
+                            depthwise_initializer=depthwise_initializer,
+                            name=name + '_dwconv')
+        self.supports_masking = True
+        
+    def call(self, inputs):
+        x = self.causal_pad(inputs)
+        x = self.dw_conv(x)
+        return x
+
+def Conv1DBlock(channel_size,
+          kernel_size,
+          dilation_rate=1,
+          drop_rate=0.4,
+          expand_ratio=2,
+          se_ratio=0.25,
+          activation='swish',
+          name=None):
+    '''
+    efficient conv1d block, @hoyso48
+    '''
+    if name is None:
+        name = str(tf.keras.backend.get_uid("mbblock"))
+    # Expansion phase
+    def apply(inputs):
+        channels_in = tf.keras.backend.int_shape(inputs)[-1]
+        channels_expand = channels_in * expand_ratio
+
+        skip = inputs
+
+        x = tf.keras.layers.Dense(
+            channels_expand,
+            use_bias=True,
+            activation=activation,
+            name=name + '_expand_conv')(inputs)
+
+        # Depthwise Convolution
+        x = CausalDWConv1D(kernel_size,
+            dilation_rate=dilation_rate,
+            use_bias=False,
+            name=name + '_dwconv')(x)
+
+        x = tf.keras.layers.BatchNormalization(momentum=0.95, name=name + '_bn')(x)
+
+        x  = ECA()(x)
+
+        x = tf.keras.layers.Dense(
+            channel_size,
+            use_bias=True,
+            name=name + '_project_conv')(x)
+
+        if drop_rate > 0:
+            x = tf.keras.layers.Dropout(drop_rate, noise_shape=(None,1,1), name=name + '_drop')(x)
+
+        if (channels_in == channel_size):
+            x = tf.keras.layers.add([x, skip], name=name + '_add')
+        return x
+
+    return apply
+
+class MultiHeadSelfAttention(tf.keras.layers.Layer):
+    def __init__(self, dim=256, num_heads=4, dropout=0, **kwargs):
+        super().__init__(**kwargs)
+        self.dim = dim
+        self.scale = self.dim ** -0.5
+        self.num_heads = num_heads
+        self.qkv = tf.keras.layers.Dense(3 * dim, use_bias=False)
+        self.drop1 = tf.keras.layers.Dropout(dropout)
+        self.proj = tf.keras.layers.Dense(dim, use_bias=False)
+        self.supports_masking = True
+
+    def call(self, inputs, mask=None):
+        qkv = self.qkv(inputs)
+        qkv = tf.keras.layers.Permute((2, 1, 3))(tf.keras.layers.Reshape((-1, self.num_heads, self.dim * 3 // self.num_heads))(qkv))
+        q, k, v = tf.split(qkv, [self.dim // self.num_heads] * 3, axis=-1)
+
+        attn = tf.matmul(q, k, transpose_b=True) * self.scale
+
+        if mask is not None:
+            mask = mask[:, None, None, :]
+
+        attn = tf.keras.layers.Softmax(axis=-1)(attn, mask=mask)
+        attn = self.drop1(attn)
+
+        x = attn @ v
+        x = tf.keras.layers.Reshape((-1, self.dim))(tf.keras.layers.Permute((2, 1, 3))(x))
+        x = self.proj(x)
+        return x
+
+
+def TransformerBlock(dim=256, num_heads=6, expand=4, attn_dropout=0.2, drop_rate=0.2, activation='swish'):
+    def apply(inputs):
+        x = inputs
+        x = tf.keras.layers.LayerNormalization(epsilon=1e-6)(x)
+        x = MultiHeadSelfAttention(dim=dim,num_heads=num_heads,dropout=attn_dropout)(x)
+        x = tf.keras.layers.Dropout(drop_rate, noise_shape=(None,1,1))(x)
+        x = tf.keras.layers.Add()([inputs, x])
+        attn_out = x
+
+        x = tf.keras.layers.LayerNormalization(epsilon=1e-6)(x)
+        x = tf.keras.layers.Dense(dim*expand, use_bias=False, activation=activation)(x)
+        x = tf.keras.layers.Dense(dim, use_bias=False)(x)
+        x = tf.keras.layers.Dropout(drop_rate, noise_shape=(None,1,1))(x)
+        x = tf.keras.layers.Add()([attn_out, x])
+        return x
+    return apply
+
+def positional_encoding(maxlen, num_hid):
+        depth = num_hid/2
+        positions = tf.range(maxlen, dtype = tf.float32)[..., tf.newaxis]
+        depths = tf.range(depth, dtype = tf.float32)[np.newaxis, :]/depth
+        angle_rates = tf.math.divide(1, tf.math.pow(tf.cast(10000, tf.float32), depths))
+        angle_rads = tf.linalg.matmul(positions, angle_rates)
+        pos_encoding = tf.concat(
+          [tf.math.sin(angle_rads), tf.math.cos(angle_rads)],
+          axis=-1)
+        return pos_encoding
+def CTCLoss(labels, logits):
+    label_length = tf.reduce_sum(tf.cast(labels != pad_token_idx, tf.int32), axis=-1)
+    logit_length = tf.ones(tf.shape(logits)[0], dtype=tf.int32) * tf.shape(logits)[1]
+    loss = tf.nn.ctc_loss(
+            labels=labels,
+            logits=logits,
+            label_length=label_length,
+            logit_length=logit_length,
+            blank_index=pad_token_idx,
+            logits_time_major=False
+        )
+    loss = tf.reduce_mean(loss)
+    return loss
+def get_model(dim = 384,num_blocks = 6,drop_rate  = 0.4):
+    inp = tf.keras.Input(INPUT_SHAPE)
+    x = tf.keras.layers.Masking(mask_value=0.0)(inp)
+    x = tf.keras.layers.Dense(dim, use_bias=False, name='stem_conv')(x)
+    pe = tf.cast(positional_encoding(INPUT_SHAPE[0], dim), dtype=x.dtype)
+    x = x + pe
+    x = tf.keras.layers.BatchNormalization(momentum=0.95,name='stem_bn')(x)
+    
+    for i in range(num_blocks):
+        x = Conv1DBlock(dim, 11, drop_rate=drop_rate)(x)
+        x = Conv1DBlock(dim,  5, drop_rate=drop_rate)(x)
+        x = Conv1DBlock(dim,  3, drop_rate=drop_rate)(x)
+        x = TransformerBlock(dim, expand=2)(x)
+
+    x = tf.keras.layers.Dense(dim*2,activation='relu',name='top_conv')(x)
+    x = tf.keras.layers.Dropout(drop_rate)(x)
+    x = tf.keras.layers.Dense(len(char_to_num),name='classifier')(x)
+
+    model = tf.keras.Model(inp, x)
+
+    loss = CTCLoss
+    
+    # Adam Optimizer
+    optimizer = tfa.optimizers.RectifiedAdam(sma_threshold=4)
+    optimizer = tfa.optimizers.Lookahead(optimizer, sync_period=5)
+
+    model.compile(loss=loss, optimizer=optimizer)
+
+    return model
+
+
+
+
+
+
+
+tf.keras.backend.clear_session()
+model = get_model()
+model(batch[0])
+model.summary()
