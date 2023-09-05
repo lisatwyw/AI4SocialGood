abf019446a1a4bd9d0b981d3ebae5e0d4d8878ac
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2023-09-04 21:16:23 -0700

    Update main.py

--- n2023/main.py
+++ n2023/main.py
@@ -90,21 +90,20 @@ def get_data():
     # drop the 3 cases of unknown sex and then map codes to English words
     df2 = df2[df2.sex != 0]
 
-decoded_df2 = df2.copy()
-for col in mapping.keys():
-    if col != 'disposition':
-        decoded_df2[col] = decoded_df2[col].map(mapping[col])    
-
-decoded_df = df.copy()
-for col in mapping.keys():
-    if col != 'disposition':
-        decoded_df[col] = decoded_df[col].map(mapping[col])        
-
-return decoded_df, decoded_df2
+    decoded_df2 = df2.copy()
+    for col in mapping.keys():
+        if col != 'disposition':
+            decoded_df2[col] = decoded_df2[col].map(mapping[col])    
+    
+    decoded_df = df.copy()
+    for col in mapping.keys():
+        if col != 'disposition':
+            decoded_df[col] = decoded_df[col].map(mapping[col])        
+    
+    return decoded_df, decoded_df2
 
 if ( 'org_columns' in globals())==False:
   decoded_df, decoded_df2 = get_data()
-
 
 tst_case_nums = np.setdiff1d( decoded_df2.cpsc_case_number, df.cpsc_case_number )
 trn_case_nums = np.setdiff1d( decoded_df2.cpsc_case_number, tst_case_nums )
@@ -112,3 +111,64 @@ trn_case_nums = np.setdiff1d( decoded_df2.cpsc_case_number, tst_case_nums )
 print( 'If models were to be developed, we may split into trn set of', len(trn_case_nums), 'samples and tst set of',
     len(tst_case_nums), 'samples. \n\tOverlapped indices?', np.intersect1d( tst_case_nums, trn_case_nums ), '\n\n' )
  
+i=np.intersect1d( sub.cpsc_case_number, trn_case_nums ); 
+j=np.intersect1d( sub.cpsc_case_number, tst_case_nums );
+print( len(i)+len(j), sub.shape  )
+
+sub.set_index('cpsc_case_number', inplace=True)
+
+# consumer product safety commission
+
+surv_pols = {}
+
+def meta_data():
+  k  = 'narrative'
+  kk = 'mentioned_recurrent_falls' 
+ 
+  sub1 = sub.loc[ i ]
+  sub2 = sub.loc[ j ]
+
+  # Recurrent falls
+  t='trn'
+  surv_pols[t] = pol.DataFrame(sub1).with_columns(pol.when(
+      pol.col(k).str.contains('PREV F') | 
+      pol.col(k).str.contains('RECURRENT F') | 
+      pol.col("narrative").str.contains(r'FALLEN * TIMES')).then(1).otherwise(0).alias( kk ))  
+  t='tst'
+  surv_pols[t] = pol.DataFrame(sub2).with_columns(pol.when(
+      pol.col(k).str.contains('PREV F') | 
+      pol.col(k).str.contains('RECURRENT F') | 
+      pol.col("narrative").str.contains(r'FALLEN * TIMES')).then(1).otherwise(0).alias( kk ))
+  
+meta_data()
+
+# https://www.sbert.net/docs/pretrained_models.html#sentence-embedding-models/
+def get_embeddings(sentences, pretrained="paraphrase-multilingual-mpnet-base-v2"):
+    from sentence_transformers import SentenceTransformer
+    model = SentenceTransformer(pretrained)
+    embeddings = model.encode(sentences)
+    return embeddings
+
+sentences,meta,embeddings={},{},{}
+
+t='trn'
+if os.path.isfile( f"../input/embeddings_{t}.pkl"):
+    for t in ['trn','tst']:
+        with open(  f"embeddings_{t}.pkl", 'rb') as handle:
+            meta = pickle.load(handle)    
+        embeddings[t]= meta["embeddings"]
+        sentences[t] = meta["sentences"]    
+else:
+    for t in ['tst','trn']:
+        sentences[t] = list( surv_pols[t].to_pandas()["narrative_cleaned"])  
+        embeddings[t]= get_embeddings(sentences[t])   
+        # Save
+        meta[t] = {
+            "sentences": sentences,
+            "embeddings": embeddings
+        }
+        with open( f"embeddings_{t}.pkl", 'wb') as handle:
+            pickle.dump(meta[t], handle)        
+
+
+
