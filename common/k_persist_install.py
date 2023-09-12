932458a94507976ef198c4ca64eaeb734943602d
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2023-09-11 17:37:49 -0700

    Update k_persist_install.py

--- common/k_persist_install.py
+++ common/k_persist_install.py
@@ -47,17 +47,4 @@ except:
             subprocess.run(cmd, shell=False)              
 
 sys.path.append( package_dir )      
-
-import lifelines
-import time 
-
-from selenium import webdriver 
-from selenium.webdriver import Chrome 
-from selenium.webdriver.chrome.service import Service 
-from selenium.webdriver.common.by import By 
-from webdriver_manager.chrome import ChromeDriverManager
-
-
-url = 'https://www.doaj.org/search/journals?source=%7B%22query%22%3A%7B%22bool%22%3A%7B%22must%22%3A%5B%7B%22term%22%3A%7B%22bibjson.apc.has_apc%22%3Afalse%7D%7D%2C%7B%22term%22%3A%7B%22bibjson.other_charges.has_other_charges%22%3Afalse%7D%7D%5D%7D%7D%2C%22track_total_hits%22%3Atrue%7D'
-    
-    
+ 
