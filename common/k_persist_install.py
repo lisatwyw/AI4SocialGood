fd73060cf1c9cfcad97dd64232d0925e3e72e28d
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2023-09-28 09:29:24 -0700

    Update and rename k_persist_install2.py to k_persist_install.py

--- common/k_persist_install.py
+++ common/k_persist_install.py
@@ -3,28 +3,6 @@
 exec( open('k_persist_install.py','r').read() )
 '''
 
-import sys 
-import tensorflow as tf
-gpus = tf.config.list_physical_devices('GPU'); 
-
-package_dir = '/kaggle/working/mypackages'
-
-sys.path.append( package_dir )
-if gpus:
-    package_dir += '_gpu'
-sys.path.append( package_dir )
-if gpus:    
-    try:
-        # Currently, memory growth needs to be the same across GPUs
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-        logical_gpus = tf.config.list_logical_devices('GPU')
-        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-    except RuntimeError as e:
-        # Memory growth must be set before GPUs have been initialized
-        print(e)
-      
-    
 '''
 cmds= ['pip install pytorch-tabnet --target=/kaggle/working/mypackages']
 cmds+=['pip install lifelines --target=/kaggle/working/mypackages']
@@ -36,8 +14,34 @@ cmds=['pip install webdriver-manager selenium']
 '''
 
 
-import subprocess, sys
-def install( cmds ): 
+
+def install_packages( cmds ): 
+    import subprocess, sys, os
+    import tensorflow as tf
+    gpus = tf.config.list_physical_devices('GPU'); 
+    
+    package_dir = '/kaggle/working/mypackages'
+    
+    sys.path.append( package_dir )
+    if gpus:
+        package_dir += '_gpu'
+    sys.path.append( package_dir )
+    if gpus:    
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+          
+    try:
+        sys.mkdir( package_dir )
+    except:
+        pass 
+
     print( len(cmds) )
     
     for cmd in cmds:
