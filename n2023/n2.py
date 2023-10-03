fb51017c4301da301bc7c50d665076f0ff3f0c42
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2023-10-03 08:11:30 -0700

    Update n2.py

--- n2023/n2.py
+++ n2023/n2.py
@@ -1,3 +1,15 @@
+import multiprocessing as mp
+from multiprocessing import Pool    
+
+import pandas as pd
+
+from tqdm import tqdm 
+from time import time
+from datetime import date
+today =date.today()
+
+import pickle, json
+
 try:
     import wget
 except:
@@ -6,13 +18,16 @@ except:
     import wget
 
 if ('decoded_df2' in globals())==False:
+    print('reading decoded_df2...')
+    
     decoded_df2=pd.read_csv('/kaggle/input/neiss-sentence-transform-embeddings/decoded_df2__l1.csv')
-    decoded_df2=decoded_df2.drop_duplicates('cpsc_case_number',)
-    Embeddings={}
-
+    decoded_df2=decoded_df2.drop_duplicates('cpsc_case_number',)    
     decoded_df2.to_csv('decoded_df2_unique.csv')
+    
     cpcs_nums = decoded_df2.cpsc_case_number
     size_n = decoded_df2.shape[0]
+    
+    Embeddings={}
 
 def embed( model, sentences, TF_MODEL ):             
     if TF_MODEL==1:
@@ -22,169 +37,180 @@ def embed( model, sentences, TF_MODEL ):
         embeddings = model.encode(sentences)            
     return embeddings
 
-TF_MODEL = 0
+
 
 def normalization(embeds):
     norms = np.linalg.norm(embeds, 2, axis=1, keepdims=True)
     return embeds/norms
 
-for src in [ 'narrative_cleaned','narrative' ]:    
-#for src in [ 'narrative' ]:    
-    if INTERACTIVE:        
-        inp = decoded_df2[src][:100] 
-    else:
-        inp =  decoded_df2[src] 
-    
-    if 'cleaned' not in src:        
-        inp = inp.str.lower()
+
+for src in [ 'narrative_cleaned']:#,'narrative' ]:    
+    for emb in [EMB]:            
+        try:
+            del ll
+        except:
+            pass
         
-    inp = list(inp)
-        
-    for emb in EMB:
-        starttime=time()
-        if emb<4: 
-            try:
-                from sentence_transformers import SentenceTransformer
-            except:                
-                cmd=['pip install -U sentence-transformers']; install_packages( cmd, INTERACTIVE ); 
-                from sentence_transformers import SentenceTransformer
-                
-            if emb==1:# 768
-                model = SentenceTransformer('all-mpnet-base-v2')
-            elif emb==2: # 384
-                model = SentenceTransformer('all-MiniLM-L6-v2')
-            elif emb==3: #768
-                model = SentenceTransformer('paraphrase-mpnet-base-v2')  # already explored paraphrase-multilingual-mpnet-base-v2")
-        
-        elif emb==4: # 512
-            TF_MODEL = 1
-            import tensorflow_hub as hub
-            # Daniel Cer, Yinfei Yang, Sheng-yi Kong, Nan Hua, Nicole Limtiaco, Rhomni St. John, Noah Constant, Mario Guajardo-Céspedes, Steve Yuan, Chris Tar, Yun-Hsuan Sung, Brian Strope, Ray Kurzweil. 
-            # Universal Sentence Encoder. arXiv:1803.11175, 2018.
-            module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"         
-            model = hub.load(module_url)
+        for bat_i in tqdm(list(range(nbatches)) ):
             
-        
-
-        elif emb==5:
-            # [1] Fangxiaoyu Feng, Yinfei Yang, Daniel Cer, Narveen Ari, Wei Wang. Language-agnostic BERT Sentence Embedding. July 2020
-            import tensorflow_hub as hub            
-
-            install_packages(['pip install tensorflow_text'])
-            import tensorflow_text as text  # Registers the ops.
-
-            preprocessor = hub.KerasLayer('https://tfhub.dev/google/universal-sentence-encoder-cmlm/multilingual-preprocess/2')
-            model = hub.KerasLayer('https://tfhub.dev/google/LaBSE/2')
-            inp=preprocessor(inp)  
-            TF_MODEL = 1
-
-        elif emb >=6:
-            import torch
-            from transformers import BertModel, BertTokenizerFast            
-            tokenizer = BertTokenizerFast.from_pretrained("setu4993/LEALLA-small")
-            if emb==6:
-                model = BertModel.from_pretrained("setu4993/LEALLA-small")
-            elif emb==7:
-                model = BertModel.from_pretrained("setu4993/LEALLA-base")
-            elif emb==8:
-                model = BertModel.from_pretrained("setu4993/LEALLA-large")
-                
-            model = model.eval()
-            english_inputs = tokenizer(inp, return_tensors="pt", padding=True)
-
-            with torch.no_grad():
-                english_outputs = model(**english_inputs)
-            Embeddings[emb]= np.array( english_outputs .pooler_output )
-
-        elif emb>=1006:            
-            import tensorflow_hub as hub            
-            TF_MODEL = 1            
-            if emb==6:
-                model = hub.KerasLayer("https://tfhub.dev/google/LEALLA/LEALLA-small/1")
-            elif emb==7:
-                model = hub.KerasLayer("https://tfhub.dev/google/LEALLA/LEALLA-base/1")        
+            if 0 & INTERACTIVE:        
+                inp = decoded_df2[src][:100] 
             else:
-                model = hub.KerasLayer("https://tfhub.dev/google/LEALLA/LEALLA-large/1")                        
-            print( model.summary() )
-            inp = preprocessor(inp)
+                inp =  decoded_df2[src][bat_i::nbatches]
+            if 'cleaned' not in src:        
+                inp = inp.str.lower()
+            inp = list(inp)
 
-        elif emb==9: # Roberta
-            install_packages( ['pip install -U tensorflow==2.13'], INTERACTIVE ); import tensorflow as tf; import tokenization            
-            #https://raw.githubusercontent.com/tensorflow/models/master/official/nlp/bert/tokenization.py            
-            url='https://raw.githubusercontent.com/google-research/bert/master/tokenization.py'; import wget; wget.download(url)
+            starttime=time()           
+            TF_MODEL = 0
             
-            from tensorflow.keras.models import Model
-            from tensorflow.keras.optimizers import Adam
-            from tensorflow.keras.layers import Dense, Input
+            if emb<4: 
+                try:
+                    from sentence_transformers import SentenceTransformer
+                except:                
+                    install_packages( ['pip install -U sentence-transformers'], INTERACTIVE ); 
+                    from sentence_transformers import SentenceTransformer
 
-            def bert_encode(texts, tokenizer, max_len=512):
-                all_tokens = []
-                all_masks = []
-                all_segments = []
+                if emb==1:# 768
+                    model = SentenceTransformer('all-mpnet-base-v2')
+                elif emb==2: # 384
+                    model = SentenceTransformer('all-MiniLM-L6-v2')
+                elif emb==3: #768
+                    model = SentenceTransformer('paraphrase-mpnet-base-v2')  # already explored paraphrase-multilingual-mpnet-base-v2")
 
-                for text in texts:
-                    text = tokenizer.tokenize(text)
+            elif emb==4: # 512
+                TF_MODEL = 1
+                import tensorflow_hub as hub
+                # Daniel Cer, Yinfei Yang, Sheng-yi Kong, Nan Hua, Nicole Limtiaco, Rhomni St. John, Noah Constant, Mario Guajardo-Céspedes, Steve Yuan, Chris Tar, Yun-Hsuan Sung, Brian Strope, Ray Kurzweil. 
+                # Universal Sentence Encoder. arXiv:1803.11175, 2018.
+                module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"         
+                model = hub.load(module_url)       
 
-                    text = text[:max_len-2]
-                    input_sequence = ["[CLS]"] + text + ["[SEP]"]
-                    pad_len = max_len - len(input_sequence)
+            elif emb==5:
+                # [1] Fangxiaoyu Feng, Yinfei Yang, Daniel Cer, Narveen Ari, Wei Wang. Language-agnostic BERT Sentence Embedding. July 2020
+                import tensorflow_hub as hub            
 
-                    tokens = tokenizer.convert_tokens_to_ids(input_sequence)
-                    tokens += [0] * pad_len
-                    pad_masks = [1] * len(input_sequence) + [0] * pad_len
-                    segment_ids = [0] * max_len
+                if ( 'll' in globals() )==False:
+                    ll = np.zeros( (size_n, 768) )
+                try:
+                    import tensorflow_text as text  # Registers the ops.
+                except:
+                    install_packages(['pip install tensorflow_text'], INTERACTIVE)
+                    import tensorflow_text as text  # Registers the ops.
 
-                    all_tokens.append(tokens)
-                    all_masks.append(pad_masks)
-                    all_segments.append(segment_ids)
+                preprocessor = hub.KerasLayer('https://tfhub.dev/google/universal-sentence-encoder-cmlm/multilingual-preprocess/2')
+                model = hub.KerasLayer('https://tfhub.dev/google/LaBSE/2')
 
-                return np.array(all_tokens), np.array(all_masks), np.array(all_segments)
+                inp=preprocessor(inp)  
+                
+                print( 'input preprocessed')
+                TF_MODEL = 1
+                
+                a = embed( model, inp, TF_MODEL )            
+                r=normalization( a['default']  )
+                ll[bat_i::nbatches] = np.array(r)
+            
+                print( 'calc embedding')            
 
-            def build_model(bert_layer, max_len=512):
+            elif emb>=6:                
+                if ( 'll' in globals() )==False:
+                    ll = np.zeros( (size_n, 768))
+                    
+                import torch            
+                from transformers import BertModel, BertTokenizerFast            
+                tokenizer = BertTokenizerFast.from_pretrained("setu4993/LEALLA-small")
+                if emb==6:
+                    model = BertModel.from_pretrained("setu4993/LEALLA-small")
+                elif emb==7:
+                    model = BertModel.from_pretrained("setu4993/LEALLA-base")
+                elif emb==8:
+                    model = BertModel.from_pretrained("setu4993/LEALLA-large")
 
-                input_word_ids = Input(shape=(max_len,), dtype=tf.int32, name="input_word_ids")
-                input_mask = Input(shape=(max_len,), dtype=tf.int32, name="input_mask")
-                segment_ids = Input(shape=(max_len,), dtype=tf.int32, name="segment_ids")
+                model = model.eval()
+                english_inputs = tokenizer(inp, return_tensors="pt", padding=True)
 
-                #could be pooled_output, sequence_output yet sequence output provides for each input token (in context)
-                _, sequence_output = bert_layer([input_word_ids, input_mask, segment_ids])
-                clf_output = sequence_output[:, 0, :]
-                out = Dense(1, activation='sigmoid')(clf_output)
+                with torch.no_grad():
+                    english_outputs = model(**english_inputs)
+                r = np.array( english_outputs.pooler_output )                                               
+                ll[bat_i::nbatches] = r                
+                
+            elif 0: # Roberta
+                cmd=['pip install -U tensorflow==2.13']; install_packages( cmd, INTERACTIVE ); import tensorflow as tf; import tokenization            
+                #https://raw.githubusercontent.com/tensorflow/models/master/official/nlp/bert/tokenization.py            
+                url='https://raw.githubusercontent.com/google-research/bert/master/tokenization.py';import wget; wget.download(url)
 
-                model = Model(inputs=[input_word_ids, input_mask, segment_ids], outputs=out)
+                from tensorflow.keras.models import Model
+                from tensorflow.keras.optimizers import Adam
+                from tensorflow.keras.layers import Dense, Input
 
-                #specifying optimizer
-                model.compile(Adam(learning_rate=1e-5), loss='binary_crossentropy', metrics=['accuracy'])
+                def bert_encode(texts, tokenizer, max_len=512):
+                    all_tokens = []
+                    all_masks = []
+                    all_segments = []
 
-                return model
+                    for text in texts:
+                        text = tokenizer.tokenize(text)
 
-            module_url = "https://tfhub.dev/tensorflow/bert_en_uncased_L-24_H-1024_A-16/1"
-            bert_layer = hub.KerasLayer(module_url, trainable=True)
-            model = build_model(bert_layer, )
-            TF_MODEL = 1
+                        text = text[:max_len-2]
+                        input_sequence = ["[CLS]"] + text + ["[SEP]"]
+                        pad_len = max_len - len(input_sequence)
 
-        #nprocs=mp.cpu_count() 
-        #print( nprocs, ' processes' )
+                        tokens = tokenizer.convert_tokens_to_ids(input_sequence)
+                        tokens += [0] * pad_len
+                        pad_masks = [1] * len(input_sequence) + [0] * pad_len
+                        segment_ids = [0] * max_len
+
+                        all_tokens.append(tokens)
+                        all_masks.append(pad_masks)
+                        all_segments.append(segment_ids)
+
+                    return np.array(all_tokens), np.array(all_masks), np.array(all_segments)
+
+                def build_model(bert_layer, max_len=512):
+
+                    input_word_ids = Input(shape=(max_len,), dtype=tf.int32, name="input_word_ids")
+                    input_mask = Input(shape=(max_len,), dtype=tf.int32, name="input_mask")
+                    segment_ids = Input(shape=(max_len,), dtype=tf.int32, name="segment_ids")
+
+                    #could be pooled_output, sequence_output yet sequence output provides for each input token (in context)
+                    _, sequence_output = bert_layer([input_word_ids, input_mask, segment_ids])
+                    clf_output = sequence_output[:, 0, :]
+                    out = Dense(1, activation='sigmoid')(clf_output)
+
+                    model = Model(inputs=[input_word_ids, input_mask, segment_ids], outputs=out)
+
+                    #specifying optimizer
+                    model.compile(Adam(learning_rate=1e-5), loss='binary_crossentropy', metrics=['accuracy'])
+
+                    return model
+
+                module_url = "https://tfhub.dev/tensorflow/bert_en_uncased_L-24_H-1024_A-16/1"
+                bert_layer = hub.KerasLayer(module_url, trainable=True)
+                model = build_model(bert_layer, )
+                TF_MODEL = 1
+
+        try:
+            Embeddings[emb]= ll 
+        except:
+            pass        
         nprocs = 2        
         if 0:
             with Pool( nprocs ) as pool:                   
                 Embeddings[emb] = list( tqdm( pool.imap( embed, inp ) ))
-        elif emb<6:            
+        elif emb<5:            
             Embeddings[emb] = embed( model, inp, TF_MODEL )            
-        if emb==5:             
-            r=normalization( Embeddings[emb]['default']  )
-            Embeddings[emb]=np.array(r)
-        
+
+
         exec_time = (time() - starttime)/60 
-                
+
         d=Embeddings[emb].shape[1]
         print(d,'dimensions')
-        
+
         pref = f'{src}_n{size_n}_emb{emb}_d{d}_{today}'
         f=open( pref+'.txt' ,'w')
         f.write( 'Finished in %2.f minutes'%exec_time )
         f.close()
-        
+
         with open( pref+'".pkl', 'wb') as handle:
             pickle.dump( {"embeddings": Embeddings[emb] } , handle)   
         print('src', emb, 'done in ', exec_time, 'written to', pref )                          
